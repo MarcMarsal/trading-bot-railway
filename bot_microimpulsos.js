720b7eefc9d29e95a02b7f27b90b236184790100
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-01 22:46:43 +0200

    Update bot_microimpulsos.js

--- bot_microimpulsos.js
+++ bot_microimpulsos.js
@@ -128,7 +128,7 @@ async function processSymbol(symbol, timeframe) {
 // LOOP PRINCIPAL
 // -------------------------------------------------------------
 async function mainLoop() {
-  console.log("Tick microimpulsos:", new Date().toISOString());
+  //console.log("Tick microimpulsos:", new Date().toISOString());
 
   for (const symbol of SYMBOLS) {
     for (const timeframe of TIMEFRAMES) {
@@ -141,7 +141,7 @@ async function mainLoop() {
       try {
         await processSymbol(symbol, timeframe);
       } catch (err) {
-        .error("Error processant", symbol, timeframe, err.message);
+        console.log("Error processant", symbol, timeframe, err.message);
       }
     }
   }
