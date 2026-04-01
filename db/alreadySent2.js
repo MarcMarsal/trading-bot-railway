fc2a1d919f7cf7160583ae0c684f2ad174663fe0
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-01 09:23:13 +0200

    Create alreadySent2.js

---
+++ db/alreadySent2.js
@@ -0,0 +1,18 @@
+// db/alreadySent2.js
+import { client } from "./client.js";
+
+export async function alreadySent2(symbol, timeframe, type, timestamp) {
+  const q = await client.query(
+    `
+    SELECT 1 FROM signals2
+    WHERE symbol = $1
+      AND timeframe = $2
+      AND type = $3
+      AND timestamp = $4
+    LIMIT 1
+    `,
+    [symbol, timeframe, type, timestamp]
+  );
+
+  return q.rowCount > 0;
+}
