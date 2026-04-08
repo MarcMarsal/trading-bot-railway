dc725249c45d12f25621a90b5efffe36c38fd717
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-08 11:08:14 +0200

    Create candles-sc.js

---
+++ scalping/core-sc/candles-sc.js
@@ -0,0 +1,9 @@
+// candles-sc.js
+// Llegeix veles 5m i 15m d'OKX
+
+async function getCandlesSc(symbol, timeframe, limit = 20) {
+    // TODO: crida a OKX per obtenir veles
+    return [];
+}
+
+module.exports = { getCandlesSc };
