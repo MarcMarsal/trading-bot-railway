ac207432d9ccef2bd1b95c030fddf1daf0d497e0
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-08 12:02:24 +0200

    Update bot_scalping.js

--- scalping/bot_scalping.js
+++ scalping/bot_scalping.js
@@ -1,11 +1,13 @@
-const pairs = require('./core-sc/pairs-sc');
-const { runScalping } = require('./core-sc/runner-sc');
-const db = require('../core/db');
+import { client, initDB } from '../core/db/client.js';
+import { runScalping } from './core-sc/runner-sc.js';
+import pairs from './core-sc/pairs-sc.js';
+
+await initDB(); // ← imprescindible
 
 async function loop() {
-    for (const symbol of pairs) {
-        await runScalping(db, symbol);
-    }
+  for (const symbol of pairs) {
+    await runScalping(client, symbol);
+  }
 }
 
 setInterval(loop, 60 * 1000);
