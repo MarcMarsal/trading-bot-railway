7d5dcd35a5b55224db300cd874568d4da8d92320
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-08 12:13:12 +0200

    Update pairs-sc.js

--- scalping/core-sc/pairs-sc.js
+++ scalping/core-sc/pairs-sc.js
@@ -1,25 +1,23 @@
 // pairs-sc.js
-// Llista de criptos per scalping (alta liquiditat)
-
-module.exports = [
-    'BTC-USDT',
-    'ETH-USDT',
-    'SOL-USDT',
-    'BNB-USDT',
-    'XRP-USDT',
-    'ADA-USDT',
-    'DOGE-USDT',
-    'AVAX-USDT',
-    'DOT-USDT',
-    'LINK-USDT',
-    'MATIC-USDT',
-    'LTC-USDT',
-    'ATOM-USDT',
-    'ETC-USDT',
-    'APT-USDT',
-    'ARB-USDT',
-    'OP-USDT',
-    'SUI-USDT',
-    'NEAR-USDT',
-    'FIL-USDT'
+export default [
+  'BTC-USDT',
+  'ETH-USDT',
+  'SOL-USDT',
+  'BNB-USDT',
+  'XRP-USDT',
+  'ADA-USDT',
+  'DOGE-USDT',
+  'AVAX-USDT',
+  'DOT-USDT',
+  'LINK-USDT',
+  'MATIC-USDT',
+  'LTC-USDT',
+  'ATOM-USDT',
+  'ETC-USDT',
+  'APT-USDT',
+  'ARB-USDT',
+  'OP-USDT',
+  'SUI-USDT',
+  'NEAR-USDT',
+  'FIL-USDT'
 ];
