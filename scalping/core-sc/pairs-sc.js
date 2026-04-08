14922126411779a53368661d3bc61a2a5a42ed03
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-08 11:32:14 +0200

    Create pairs-sc.js

---
+++ scalping/core-sc/pairs-sc.js
@@ -0,0 +1,25 @@
+// pairs-sc.js
+// Llista de criptos per scalping (alta liquiditat)
+
+module.exports = [
+    'BTC-USDT',
+    'ETH-USDT',
+    'SOL-USDT',
+    'BNB-USDT',
+    'XRP-USDT',
+    'ADA-USDT',
+    'DOGE-USDT',
+    'AVAX-USDT',
+    'DOT-USDT',
+    'LINK-USDT',
+    'MATIC-USDT',
+    'LTC-USDT',
+    'ATOM-USDT',
+    'ETC-USDT',
+    'APT-USDT',
+    'ARB-USDT',
+    'OP-USDT',
+    'SUI-USDT',
+    'NEAR-USDT',
+    'FIL-USDT'
+];
