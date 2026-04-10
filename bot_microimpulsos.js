d93d923d1d4cd73f0a347f05389626ac58c99629
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-10 10:32:19 +0200

    Update bot_microimpulsos.js

--- bot_microimpulsos.js
+++ bot_microimpulsos.js
@@ -19,7 +19,7 @@ const SYMBOLS = [
   "ASTER-USDT", "BCH-USDT", "VIRTUAL-USDT"
 ];
 
-const TIMEFRAMES = ["15m", "30m", "1H"];
+const TIMEFRAMES = ["30m", "1H"];
 
 // -------------------------------------------------------------
 // GET CANDLES FROM DB (PG)
