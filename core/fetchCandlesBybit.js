f38af6691fa5319e160b2187b75f796b626cf5e2
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-15 17:03:50 +0200

    Update fetchCandlesBybit.js

--- core/fetchCandlesBybit.js
+++ core/fetchCandlesBybit.js
@@ -1,25 +1,18 @@
-// core/fetchCandlesBybit.js
-
 import axios from "axios";
+import { client } from "../db/client.js";
 
-export async function fetchCandlesBybit(symbol) {
+export async function fetchAndStoreCandlesBybit(symbol, timeframe) {
   const cleanSymbol = symbol.replace("-", "").toUpperCase();
-
-  // Proxy Render
   const url = "https://bybit-proxy-18mg.onrender.com";
-
-  console.log("URL utilitzada:", url);
 
   const res = await axios.get(url, {
     params: {
       symbol: cleanSymbol,
       interval: "60",
-      limit: 3
+      limit: 200
     },
     timeout: 8000
   });
-
-  console.log("Resposta del Proxy:", JSON.stringify(res.data, null, 2));
 
   if (!res.data?.result?.list) {
     throw new Error("Resposta Bybit incorrecta");
@@ -27,7 +20,7 @@ export async function fetchCandlesBybit(symbol) {
 
   const list = res.data.result.list;
 
-  return list.map(c => ({
+  const candles = list.map(c => ({
     timestamp: Number(c[0]),
     open: Number(c[1]),
     high: Number(c[2]),
@@ -35,4 +28,27 @@ export async function fetchCandlesBybit(symbol) {
     close: Number(c[4]),
     volume: Number(c[5]),
   })).reverse();
+
+  for (const c of candles) {
+    await client.query(
+      `
+      INSERT INTO candles2
+        (symbol, timeframe, open, high, low, close, volume, timestamp)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      ON CONFLICT (symbol, timeframe, timestamp) DO NOTHING
+      `,
+      [
+        symbol,
+        timeframe,
+        c.open,
+        c.high,
+        c.low,
+        c.close,
+        c.volume,
+        c.timestamp
+      ]
+    );
+  }
+
+  return candles;
 }
