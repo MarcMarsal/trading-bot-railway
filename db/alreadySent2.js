65670d07efa4c9586775b371c56122019ba3bf65
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-26 09:13:51 +0200

    Update alreadySent2.js

--- db/alreadySent2.js
+++ db/alreadySent2.js
@@ -1,30 +1,18 @@
 // db/alreadySent2.js
-
 import { client } from "./client.js";
 
-export async function alreadySent2(symbol, timeframe, type, entry, dayKey, status = null) {
-  if (!dayKey) {
-    console.log("❌ alreadySent2() ERROR — dayKey invalid:", dayKey);
-    return false;
-  }
-
-  let query = `
+export async function alreadySent2(symbol, timeframe, type, timestamp) {
+  const query = `
     SELECT 1 FROM signals2
     WHERE symbol = $1
       AND timeframe = $2
       AND type = $3
-      AND entry = $4
-      AND LEFT(date_es, 10) = $5
+      AND timestamp = $4
+    LIMIT 1
   `;
 
-  const params = [symbol, timeframe, type, entry, dayKey];
-
-  if (status) {
-    query += ` AND status = $6 `;
-    params.push(status);
-  }
-
-  query += ` LIMIT 1`;
+  const params = [symbol, timeframe, type, timestamp];
   const q = await client.query(query, params);
   return q.rowCount > 0;
 }
+
