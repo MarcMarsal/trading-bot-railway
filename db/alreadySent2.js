357866c5be695801437122fe18ac89ed8af479ee
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-04-30 12:24:25 +0200

    Update alreadySent2.js

--- db/alreadySent2.js
+++ db/alreadySent2.js
@@ -2,18 +2,18 @@
 import { client } from "./client.js";
 
 export async function alreadySent2(symbol, timeframe, type, timestampMs) {
-  const tsSec = Math.floor(Number(timestampMs) / 1000);
+  const tsMs = Number(timestampMs);
 
   const query = `
     SELECT 1 FROM signals2
     WHERE symbol = $1
       AND timeframe = $2
       AND type = $3
-      AND timestamp = $4
+      AND timestamp_ms = $4
     LIMIT 1
   `;
 
-  const params = [symbol, timeframe, type, tsSec];
+  const params = [symbol, timeframe, type, tsMs];
   const q = await client.query(query, params);
 
   return q.rowCount > 0;
