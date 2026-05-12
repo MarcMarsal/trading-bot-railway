e59e251005330c7fb6f2ce9aa3ec69b8b68e511b
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-05-12 10:37:06 +0200

    Update patterns.js

--- core/patterns.js
+++ core/patterns.js
@@ -70,7 +70,7 @@ export async function detectMSES(candlesRaw, symbol, timeframe) {
 
     let trendSignal = 0;
 
-    if (i >= bars12h) {
+    if (i >= bars12h + 1) {
       const closesNowCalc = closes.slice(i - bars12h, i);
       const closesPastCalc = closes.slice(i - bars12h * 2, i - bars12h);
 
@@ -127,7 +127,7 @@ export async function detectMSES(candlesRaw, symbol, timeframe) {
         windowNow, windowPast, highNow, highPast,
         lowNow, lowPast, closeNow, closePast, targetTs;
 
-    if (i >= bars12h) {
+    if (i >= bars12h + 1) {
       pastIndex = i - bars12h;
       targetTs = candles[i - bars12h].timestamp;
 
