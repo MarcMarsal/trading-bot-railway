376d149b78e4fd7400dcbffdf248284d524ade6c
MarcMarsal <59292333+MarcMarsal@users.noreply.github.com>
2026-06-08 07:37:35 +0200

    Update patterns.js

--- core/patterns.js
+++ core/patterns.js
@@ -251,16 +251,14 @@ export async function detectMSES(candlesRaw, symbol, timeframe) {
    
     
     if (esNew) {
-       // inici
+      // inici
       // 🟩 FIAT — BLOQUEIG DE REGENERACIÓ DE SENYALS
-      //const tfMinutes = timeframe === "1H" ? 60 : 240;
-      const nowCandle = Math.floor(Date.now() / (tfMinutes * 60 * 1000));
-      const signalCandle = Math.floor(c1.timestamp / (tfMinutes * 60 * 1000));
+      const lastCandleTs = candles[n - 1].timestamp;
+      const signalTs = c1.timestamp;
 
-      if (signalCandle !== nowCandle) {
-        continue; // NO generar senyals antigues
+      if (signalTs !== lastCandleTs) {
+        continue;
       }
-
       // fi
       signals.push({
         symbol,
